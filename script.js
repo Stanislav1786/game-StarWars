99a8b7782559e7c26a40c5059085b8384d34429a
Stanislav1786 <ogonek1786@gmail.com>
2020-04-09 09:39:59 +0300

    Изменен порядок карт

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ var resetBtn = document.querySelector("#reset-btn");
 var countCards = 16;
 
 var images = [
-    1, 2, 3, 4,
-    5, 6, 7, 8,
+    6, 2, 1, 5,
+    4, 7, 3, 8,
     2, 5, 1, 6,
     8, 7, 3, 4,
 ];
